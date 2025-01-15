13f02f30c765d9b85af4e697cafcfb46bc6fc049
Shubh-Dev <shubh.scb@gmail.com>
2025-01-15 21:54:49 +0530

    feat: form component in progress

--- src/components /From.tsx
+++ src/components /From.tsx
@@ -3,5 +3,24 @@ import Input from './Input';
 
 interface formProps {
     formType: 'login' | 'signup' | 'reset';
-    onSubmit: (data: {email: string; password: string}) => void;
+    onSubmit: (data: {email: string; password?: string; confirmPassword?: string}) => void;
 }
+
+const Form: React.FC<formProps> = ({formType, onSubmit}) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        const formData: {email: string; password?: string; confirmPassword?: string} = {email};
+
+        if (formType == "login" || formType == 'signup' || formType == "reset") formData.password = password;
+        if (formType == "signup" || formType == "reset") formData.confirmPassword = confirmPassword;
+        onSubmit(formData);
+    };
+
+    return()
+}
+
+export default Form;
