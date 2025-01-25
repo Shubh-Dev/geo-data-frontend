94c75f9046c491b12ab649201fd73330f1096914
Shubh M Das <shubh.scb@gmail.com>
2025-01-25 05:18:55 +0000

    feat: navigation progress

---
+++ src/utils/navigation.ts
@@ -0,0 +1,20 @@
+export const mainNavItems = [
+    {
+        itemName: "Home",
+        active: false,
+        to: "/",
+        activeNavItem: "index",
+    },
+    {
+        itemName: "Contact Us",
+        active: false,
+        to: "/contact",
+        activeNavItem: "contact",
+    },
+    {
+        itemName: "Sig Out",
+        active: false,
+        to: "/auth/signout",
+        activeNavItem: "signout",
+    },
+];
